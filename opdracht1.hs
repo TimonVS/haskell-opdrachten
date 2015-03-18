6c9aa340e64b446978c2a29097e211a5586c4e06
Schamper <mrwafflewaffle@aim.com>
2015-03-18 13:55:47 +0100

    intersection en disjunction

--- opdracht1.hs
+++ opdracht1.hs
@@ -7,20 +7,44 @@ import Data.List.Split
 isDigit' :: Char -> Bool
 isDigit' x = x `elem` ['0'..'9']
 
+-- 1.
 containsDecimal :: String -> Bool
 containsDecimal [] = False
 containsDecimal (x:xs)
 	| isDigit x = True
 	| otherwise = containsDecimal xs
 
+-- 2.
 getDecimalsAsString :: String -> [String]
 getDecimalsAsString xs = wordsBy (\x -> not (isDigit' x) .|. isSeparator x) xs
 
 getDecimals :: String -> [Int]
 getDecimals xs = map read $ (getDecimalsAsString xs) :: [Int]
 
+-- 3.
 decimalCount :: String -> Int
 decimalCount xs = length (getDecimalsAsString xs)
 
+-- 4.
 longestDecimal :: String -> Int
 longestDecimal xs = read $ (maximum (getDecimalsAsString xs)) :: Int
+
+-- 5.
+
+-- 6.
+unique :: (Eq a) => [a] -> [a]
+unique xs = unique' xs []
+	where
+		unique' [] _ = []
+		unique' (x:xs) ls
+			| x `elem` ls = unique' xs ls
+			| otherwise = x : unique' xs (x:ls)
+
+intersection :: String -> String -> String
+intersection xs ys = unique [x | x <- xs, x `elem` ys]
+
+-- 7.
+disjunction :: String -> String -> String
+disjunction xs ys = (uniqueChars xs ys) ++ (uniqueChars ys xs)
+	where
+		uniqueChars as bs = unique [a | a <- as, not (a `elem` bs)]
