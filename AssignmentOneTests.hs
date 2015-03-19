9fce1197f0b1c0988b6352bf5f171f3503bafd36
Timon van Spronsen <timonvanspronsen@outlook.com>
2015-03-19 16:00:41 +0100

    Add tests

--- AssignmentOneTests.hs
+++ AssignmentOneTests.hs
@@ -14,3 +14,27 @@ getDecimalsTest = getDecimals "Hallo123 456 test78" == [123,456,78]
 
 -- Test 3.
 decimalCountTest = decimalCount "Hallo123 456 test78" == 3
+
+-- Test 4.
+longestDecimalTest = longestDecimal "Hallo123 456 test78 9101112" == 7
+
+-- Test 5.
+maxDecimalTest = maxDecimal "Hallo123 456 test78 9101112" == 9101112
+
+-- Test 6.
+intersectionTest = intersection "programming" "software" == "roa"
+
+-- Test 7.
+disjunctionTest = disjunction "programming" "software" == "pgminsftwe"
+
+-- Test 8.
+isPermutationTest = isPermutation [1,2,4,5] [2,4,5,1] == True
+
+-- Test 9.
+equalCountTest = equalCount "programming" "software" == "ao"
+
+-- Test 10.
+elkenTest = elken "ABCD" == ["ABCD", "BD", "C", "D"]
+
+-- Test 11.
+locMaxTest = locMax [2,9,5,6,1] == [9,6]
