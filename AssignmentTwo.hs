b21e5e6c0b1cde0e78198351a278b3da8d51cba8
Schamper <mrwafflewaffle@aim.com>
2015-04-07 11:56:18 +0200

    Evalueer functie

--- AssignmentTwo.hs
+++ AssignmentTwo.hs
@@ -10,6 +10,8 @@ data BExpr = BWaarde Bool
 
 data Expr = BExpr BExpr | IExpr IExpr
 
+
+-- 1.
 zetom :: Expr -> [VStackExpr]
 zetom (BExpr x) = zetom' x
 	where 
@@ -21,3 +23,9 @@ zetom (IExpr x) = zetom' x
 		zetom' (Main.IWaarde x) = [IPush x]
 		zetom' (Main.Telop x y) = (zetom' x) ++ (zetom' y) ++ [VStack.Telop]
 		zetom' (Main.Vermenigvuldig x y) = (zetom' x) ++ (zetom' y) ++ [VStack.Vermenigvuldig]
+
+
+-- 2.
+evalueer :: Expr -> Either String VStackWaarde
+evalueer = vStack . zetom
+
